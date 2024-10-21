81b7f6130d8df3d5394ca7d486e4a0cd59cbe4ff
Ramagiri Suraj kumar <ramagirisurajkumar@gmail.com>
2024-10-22 00:47:49 +0530

    Removed authentication logic and adjusted related files

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,18 +1,6 @@
-const jwt = require('jsonwebtoken'); 
-
 module.exports = function (req, res, next) {
-     res.setHeader('X-Content-Type-Options', 'nosniff');
-
-    const token = req.header('x-auth-token');
-    if (!token) {
-         return res.redirect('/video');
-    }
-
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded.user;
-        next();
-    } catch (err) {
-         return res.redirect('/video');
-    }
+    res.setHeader('X-Content-Type-Options', 'nosniff');
+    
+    // Allow the request to proceed without authentication
+    next();
 };
